a12486ef9f788a66d1629b891c27d316a6408b42
PetrifiedLasagna <socramthesamurai@yahoo.com>
2016-07-06 00:53:47 -0400

    Added styles and code for notes

--- js/virtuoso.js
+++ js/virtuoso.js
@@ -17,8 +17,58 @@ var objTimeDenominator;
 var objPiano;
 var objPianoKeys;
 
-var prevPressed = [];
+var objNoteContainer;
+var objNotePaths;
+var objNotes = [];
 
+var noteSpr = function(note){
+  this.noteRef = note;
+  var htmlRef = document.createElement("div");
+  htmlRef.className = "note";
+  htmlRef.style.backgroundColor = "#22FF22";
+  htmlRef.style.borderColor = "#00DD00";
+  this.htmlRef = htmlRef;
+  objNotePaths[note.note].appendChild(htmlRef);
+};
+
+noteSpr.prototype.update = function(time){
+  var startTime = this.noteRef.startTime;
+  var endTime = this.noteRef.endTime;
+
+  var front = 0;
+  var back = 0;
+
+  if(endTime != -1){
+    back = 1 - Math.max(0.0, Math.min(1.0, (endTime - time) / 2.0));
+  }
+
+  front = Math.max(0.0, Math.min(1.0, (startTime - time) / 2.0));
+
+  this.htmlRef.style.bottom = front * 100 + "%";
+  this.htmlRef.style.top = back * 100 + "%";
+};
+
+noteSpr.prototype.clear = function(){
+  this.htmlRef.remove();
+};
+
+function drawNotes(time){
+  for(var i = 0; i < objNotes.length; i++){
+    if(objNotes[i].noteRef){
+      if(objNotes[i].noteRef.endTime < time){
+        objNotes[i].update(time);
+      } else {
+        objNotes[i].clear();
+        objNotes.splice(i, 1);
+      }
+    } else {
+      objNotes[i].clear();
+      objNotes.splice(i, 1);
+    }
+  }
+}
+
+var prevPressed = [];
 function draw(){
   var t = audioController.getTime();
   var notes = midiController.activeNotes;
@@ -27,6 +77,9 @@ function draw(){
   for(var i = 0; i < notes.length; i++){
     var note = notes[i];
     if(note){
+      if( (objNotes.find(function(element){return (note === element);}) == undefined) ){
+        objNotes.push(new noteSpr(note));
+      }
       if(note.startTime <= t && (note.endTime == -1 || note.endTime > t)){
         pressed.push(note.note);
         objPianoKeys[note.note].style.backgroundColor = "#22FF22";
@@ -34,7 +87,6 @@ function draw(){
         if(foundKey != -1){
           prevPressed.splice(foundKey, 1);
         }
-        continue;
       }
     }
   }
@@ -44,6 +96,17 @@ function draw(){
   }
 
   prevPressed = pressed;
+
+  //drawNotes(t);
+  //console.log(objNotes.length);
+  for(var i = 0; i < objNotes.length; i++){
+    if(objNotes[i].noteRef.endTime > t){
+      objNotes[i].update(t);
+    } else {
+      objNotes[i].clear();
+      objNotes.splice(i, 1);
+    }
+  }
 
   requestAnimationFrame(draw);
 }
@@ -115,6 +178,7 @@ function init(){
   objFile = document.getElementById("file");
   objPlaybtn = document.getElementById("play");
   objPiano = document.getElementById("piano");
+  objNoteContainer = document.getElementById("note-container");
   objTempo = document.getElementById("tempo");
   objTimeNumerator = document.getElementById("time-numerator");
   objTimeDenominator = document.getElementById("time-denominator");
@@ -131,15 +195,27 @@ function init(){
   document.body.appendChild(objDialogContainer);
 
   //"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+  objNotePaths = [];
   objPianoKeys = [];
   var keyType = 0;
   for(var i = 0; i < 128; i++){
     var pKey = document.createElement("div");
+    var pNote = document.createElement("div");
+
     pKey.className = "key key" + keyType;
+    pNote.className = "key key" + keyType + " note-path";
+
     pKey.id = "key-" + i;
+    pNote.id = "noteC-" + i;
+
     pKey.index = i;
+    pNote.index = i;
+
     objPianoKeys.push(pKey);
     objPiano.appendChild(pKey);
+
+    objNotePaths.push(pNote);
+    objNoteContainer.appendChild(pNote);
 
     if(!(i % 12 == 4 || i % 12 == 11)){
       keyType = keyType ^ 1;
@@ -159,11 +235,11 @@ function init(){
 
     reader.onload = function(raw){
       var msg = midiController.loadMidi(raw);
-      console.log(msg);
+      //console.log(msg);
       if(msg != ""){
-        displayDialogue("<p>There was a problem loading your file!</p><br/><p>" + msg + "</p>", "Ok");
+        displayDialogue("<p>There was a problem loading your file!</p><br/><p>" + msg + "</p>", "OK");
       } else {
-        hideDialogue();
+        displayDialogue("<p>Your file was succsessfully loaded!</p>", "OK");
       }
     };
 
